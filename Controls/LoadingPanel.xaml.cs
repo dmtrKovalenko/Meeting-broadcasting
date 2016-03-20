652c7de338c516b535995c6749fb5f07e6e120dc
Quint <dmtr.kovalenko@outlook.com>
2016-03-20 09:43:32 +0200

    Relese commit

---
+++ Controls/LoadingPanel.xaml.cs
@@ -0,0 +1,53 @@
+﻿//  --------------------------------
+//  Copyright (c) Huy Pham. All rights reserved.
+//  This source code is made available under the terms of the Microsoft Public License (Ms-PL)
+//  http://www.opensource.org/licenses/ms-pl.html
+//  ---------------------------------
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace thurst_media_player.Controls
+{
+    /// <summary>
+    /// Interaction logic for LoadingPanel.xaml
+    /// </summary>
+    public partial class LoadingPanel
+    {
+        public static readonly DependencyProperty IsLoadingProperty =
+            DependencyProperty.Register("IsLoading", typeof(bool), typeof(LoadingPanel), new UIPropertyMetadata(false));
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(LoadingPanel), new UIPropertyMetadata("Loading..."));
+
+        public static readonly DependencyProperty SubMessageProperty =
+            DependencyProperty.Register("SubMessage", typeof(string), typeof(LoadingPanel), new UIPropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty ClosePanelCommandProperty =
+            DependencyProperty.Register("ClosePanelCommand", typeof(ICommand), typeof(LoadingPanel));
+
+        public LoadingPanel()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public string SubMessage
+        {
+            get { return (string)GetValue(SubMessageProperty); }
+            set { SetValue(SubMessageProperty, value); }
+        }
+
+    }
+}
---
+++ Controls/MarqueeTextControl.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace thurst_media_player.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для MarqueeTextControl.xaml
+    /// </summary>
+    public partial class MarqueeText : UserControl
+    {
+        public String MarqueeContent
+        {
+            set
+            {
+                tbmarquee.Text = value;
+
+            }
+        }
+
+        private bool _isMarquing = false;
+
+        public bool IsMarquing
+        {
+            get { return _isMarquing; }
+            set
+            {
+                if (value != IsMarquing)
+                {
+                    _isMarquing = value;
+                    RightToLeftMarquee();
+                }
+            }
+        }
+
+        private double _marqueeTimeInSeconds;
+
+        public double MarqueeTimeInSeconds
+        {
+            get { return _marqueeTimeInSeconds; }
+            set
+            {
+                _marqueeTimeInSeconds = value;
+            }
+        }
+
+        public MarqueeText()
+        {
+            InitializeComponent();
+            canMain.Width = this.Width;
+            this.Loaded += new RoutedEventHandler(MarqueeText_Loaded);
+            canMain.Height = this.Height;
+        }
+
+        void MarqueeText_Loaded(object sender, RoutedEventArgs e)
+        {
+            RightToLeftMarquee();
+        }
+
+        private void RightToLeftMarquee()
+        {
+            if (!IsMarquing)
+            {
+                this.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+                DoubleAnimation doubleAnimation = new DoubleAnimation();
+                doubleAnimation.From = -canMain.ActualWidth;
+                doubleAnimation.To = canMain.ActualWidth;
+                doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+                doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(_marqueeTimeInSeconds));
+                tbmarquee.BeginAnimation(Canvas.RightProperty, doubleAnimation);
+            }
+        }
+    }
+}
+
